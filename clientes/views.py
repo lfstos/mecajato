d8ada8c8806c9d3c6987e7835630cffad1adf87d
Luiz Fernando <luiz-fernando@outlook.com>
2023-03-27 15:22:49 -0300

    Adicionada view atualiza_cliente

--- clientes/views.py
+++ clientes/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Cliente, Carro
 import re
+import json
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Cliente, Carro
+from django.core import serializers
+
 
 # Create your views here.
 
 
 def clientes(request):
     if request.method == "GET":
-        return render(request, 'clientes.html')
+        clientes_list = Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes': clientes_list})
     elif request.method == "POST":
         nome = request.POST.get('nome')
         sobrenome = request.POST.get('sobrenome')
@@ -33,13 +37,16 @@ def clientes(request):
         if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
             context['cpf'] = cpf
             return render(request, 'clientes.html', context)
-
         cliente = Cliente(nome=nome, sobrenome=sobrenome, email=email, cpf=cpf)
-
         cliente.save()
-
         for carro, placa, ano in zip(carros, placas, anos):
             car = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
             car.save()
-
         return HttpResponse('teste')
+
+
+def atualiza_cliente(request):
+    cliente_id = request.POST.get('id_cliente')
+    cliente = Cliente.objects.filter(id=cliente_id)
+    cliente_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
+    return JsonResponse(cliente_json)
